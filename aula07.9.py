6e75af610fbeac75a7a0dab2d7eb6ae7a3086a77
Maria Clara Rosa Meier <maria.clara.rosa.meier@gmail.com>
2022-09-09 09:17:11 -0300

    ajuste exercicio

--- aula07.9.py
+++ aula07.9.py
@@ -1,4 +1,4 @@
-prod = float(input('Qual o valor deste produto? '))
+prod = float(input('Qual o valor deste produto? R$'))
 desc = prod * 0.05
 total = prod - desc
-print('Este produto tem um desconto de 5%, portanto passou de {} para {}! Aproveite!'.format(prod, total))
+print('Este produto tem um desconto de 5%, portanto passou de {} reais para {} reais! Aproveite!'.format(prod, total))
